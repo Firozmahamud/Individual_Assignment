da60f78a393302b21c71eb3e3dc34fb00db56765
Firozmahamud <firozmahmud593@gmail.com>
2020-04-12 20:25:09 +0600

    M1F1

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>welcome page</title>
+</head>
+<body>
+
+<h1> welcome to laravel app </h1>
+
+</body>
+</html>
---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    
+<form method="POST" >
+
+<center>
+    <table>
+    <h2> login here</h2>
+        <tbody>
+            <tr>
+                <td>
+                   
+                    <label for="">E-mail:</label>
+                    <input type="email"  name="u_email" required placeholder="Enter your E-mail address.">
+                    
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                
+                    <label for="">Password:</label>
+                    <input type="password"  name="u_pass" required placeholder="Enter your password.">
+                   
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                   <button type="submit">Login</button>
+                   
+                    
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    </center>
+    <!-- </div> -->
+</form>
+
+</body>
+</html>
